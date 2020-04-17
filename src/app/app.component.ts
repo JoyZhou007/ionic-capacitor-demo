a9b9a0152ba1442df0b48381c60e348cad638e7d
JoyZhou <huan.zhou@zeiss.com>
2020-04-17 13:36:18 +0800

    feat: init app

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,36 @@
-import { Component } from '@angular/core';
+import { Component, AfterViewInit } from '@angular/core';
+import { Plugins, Capacitor } from '@capacitor/core';
+const { AwesomePlugin } = Plugins;
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
+export class AppComponent implements AfterViewInit {
   title = 'ionic-capacitor-demo';
+
+  constructor() {
+    if (Capacitor.isPluginAvailable('NFC')) {
+    }
+    console.log(
+      'AwesomePluginPlugin',
+      AwesomePlugin,
+      Capacitor.isPluginAvailable('AwesomePlugin')
+    );
+    AwesomePlugin.echo({ value: 'teet' });
+    AwesomePlugin.testEvent();
+    AwesomePlugin.storeContact({
+      yourName: 'joy',
+      address: { province: 'jiangsu' },
+      isAwesome: true,
+      id: 1
+    });
+  }
+  
+  ngAfterViewInit(): void {
+    window.addEventListener("myCustomEvent", function(e) {
+      console.log("myCustomEvent was fired",e)
+    });
+  }
 }
